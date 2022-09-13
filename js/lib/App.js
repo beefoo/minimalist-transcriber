e347920de09a4b2a0b66e17c0b716f5c95b9387f
beefoo <brian@youaremyjoy.org>
2022-09-13 13:20:17 -0400

    add progress message on loading, move file listener to App

--- js/lib/App.js
+++ js/lib/App.js
@@ -7,5 +7,19 @@ class App {
 
   init() {
     this.audioManager = new AudioManager();
+    this.loadListeners();
+  }
+
+  loadListeners() {
+    $('#fileinput').on('change', (e) => this.onFileInput(e));
+  }
+
+  onFileInput(event) {
+    const el = event.currentTarget;
+    if (!el.files || el.files.length <= 0) return;
+
+    const [file] = el.files;
+    console.log(file);
+    this.audioManager.loadSoundFromFile(file);
   }
 }
