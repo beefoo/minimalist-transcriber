24bd95922f76e19094fee14fbc33f31aec343c83
Brian Foo <brian@youaremyjoy.org>
2022-09-12 22:08:43 -0400

    Load audio from file input

--- js/lib/App.js
+++ js/lib/App.js
@@ -2,10 +2,10 @@ class App {
   constructor(options = {}) {
     const defaults = {};
     this.options = _.extend({}, defaults, options);
+    this.init();
   }
 
   init() {
-    this.initialized = true;
-    this.$el = $('#app');
+    this.audioManager = new AudioManager();
   }
 }
